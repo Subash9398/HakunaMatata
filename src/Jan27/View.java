7763e97b51a4025ca2e0ccb2e54ae0d3511811c5
SUBASH <subash9398@gmail.com>
2020-01-27 18:15:26 +0530

    View

--- src/Jan27/View.java
+++ src/Jan27/View.java
@@ -25,10 +25,5 @@ public class View implements IView {
 		System.out.println(result);
 	}
 
-	public static void main(String args[]) {
-		
-		View view=new View();
-		view.setPresenter(new Presenter(view,new Model()));
-		
-	}
+	
 }
